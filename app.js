de629262e662d331d29413ef8131d927db1c019f
Apple <apple@Apples-MacBook-Pro.local>
2020-05-22 04:45:38 +0530

    Final

--- app.js
+++ app.js
@@ -114,10 +114,8 @@ app.post("/delete",function(req,res){
 })
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 8000;
+  port = 3000;
 }
-app.listen(port);
-
 app.listen(port, function() {
-  console.log("Server running at port 3000.");
+  console.log("Server running.");
 })
